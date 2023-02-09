e231e161e431dd44ee51601cbaff8e50abb13eec
Matt Ogle <ogle.matt@gmail.com>
2023-02-09 14:23:29 -0600

    Update duration-to-human.pipe.ts

--- projects/luxon-angular/src/lib/formatting/duration-to-human.pipe.ts
+++ projects/luxon-angular/src/lib/formatting/duration-to-human.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Duration, ToHumanDurationOptions } from 'luxon';
+import { Duration, DurationOptions } from 'luxon';
 
 @Pipe({
   name: 'durationToHuman'
 })
 export class DurationToHumanPipe implements PipeTransform {
-  transform <T extends Duration | null | undefined>(value: T, opts?: ToHumanDurationOptions) {
+  transform <T extends Duration | null | undefined>(value: T, opts?: DurationOptions) {
     return (value == null ? null : value.toHuman(opts)) as T extends Duration ? string : null;
   }
 }
